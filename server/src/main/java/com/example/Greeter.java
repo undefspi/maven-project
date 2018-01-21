6f610be5be96d38b5f0c08c71345896ef73e4fa5
undefspi <chris.beech@wavelytics.co.uk>
2018-01-21 19:10:54 +0000

    fix one checkstyle issues

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,8 +14,8 @@ public class Greeter {
 
   /**
    * a method that greets you with the name of someone
-   * @param someone
-   * @return
+   * @param someone this is someone
+   * @return a string of the greeting
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
